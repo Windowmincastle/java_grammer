ca907565a5d5df8e49527c70c0153e9b7d1cccda
mincastle <kcm0408@naver.com>
2024-06-05 15:43:11 +0900

    0605

---
+++ src/C01Basic/C00HelloWorld.java
@@ -0,0 +1,8 @@
+package C01Basic;
+
+public class C00HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//
+//        10 15
+//        5 1 3 5 10 7 4 9 2 8
+        int n = scan.nextInt();
+        int s = scan.nextInt();
+
+        int[] numss = new int[10];
+        int[] nums = new int[n+1];
+        for(int i = 0; i < n; i++) {
+            nums[i] = scan.nextInt();
         }
+
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int total = 0;
+
+        while(start <= n && end <= n) {
+
+            if(total >= s && min > end - start) min = end - start;
+
+            if(total < s){
+                total += nums[end++];
+            }
+            else {
+                total -= nums[start++];
+            }
+
+        }
+
+        if(min == Integer.MAX_VALUE) System.out.println("0");
+        else System.out.println(min);
     }
 }
